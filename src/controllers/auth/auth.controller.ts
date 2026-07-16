d4ccedec4f1f9acb7c503eb31baae8d8467a9807
KUNDAN1334 <kundan.22bcon1334@jecrcu.edu.in>
2026-07-16 23:02:16 +0530

    implemented forgot and reset password functionality

--- src/controllers/auth/auth.controller.ts
+++ src/controllers/auth/auth.controller.ts
@@ -6,7 +6,8 @@ import jwt from 'jsonwebtoken';
 import { sendEmail } from "../../lib/email";
 import { checkPassword } from "../../lib/hash";
 import { createAccessToken , createRefreshToken, verifyRefreshToken} from "../../lib/token";
-
+import crypto  from "crypto";
+ 
 function getAppURL(){
     return process.env.APP_URL || `http://localhost:${process.env.PORT}`
 }
@@ -229,4 +230,89 @@ export async function logoutHandler(req:Request,res:Response){
     return res.status(200).json({
         message:"User logged out successfully"
     })
+}
+
+export async function forgotPasswordHandler(req:Request,res:Response){
+    const {email}=req.body as {email?:string};
+    if(!email){
+        return res.status(400).json({message : "Email is Required"});
+    }
+    const normalizedEmail=email?.toLocaleLowerCase().trim();
+    
+    try {
+        const user=await User.findOne({email : normalizedEmail});
+        if(!user){
+            return res.status(403).json({message : "If an account with this email exists, we will send you reset link"});
+        }
+
+        const rawToken=crypto.randomBytes(32).toString('hex');
+        const tokenHash=crypto.createHash('sha256').update(rawToken).digest('hex');
+        
+        user.resetPasswordToken=tokenHash;
+        user.resetPasswordExpires=new Date(Date.now()+15*60*1000);
+
+        await user.save();
+
+        const resetURL= `${getAppURL()}/auth/reset-password?token=${rawToken}`;
+
+        await sendEmail(
+            user.email,
+            "Reset Your Password",
+            `
+            <p>Click on the below link to reset the password</p>
+            <p><a href="${resetURL}">${resetURL}</a></p>
+            `
+        )
+        return res.status(403).json({message : "If an account with this email exists, we will send you reset link"});
+
+    } catch(err){
+        console.log(err);
+        res.status(500).json({
+            message: 'internal server error'
+    })
+    }
+  
+
+}
+
+
+export async function resetPasswordHandler(req:Request,res:Response){
+    const {token,password}=req.body as {token:string,password:string};
+
+    if(!token){
+        return res.status(400).json({message:"Reset token is missing"});
+    }
+    if(password && password.length<6){
+        return res.status(400).json({message: "Password is atleast 6 character long"});
+    }
+
+    try {
+        const tokenHash=crypto.createHash("sha256").update(token).digest('hex');
+        const user=await User.findOne({resetPasswordToken:tokenHash,
+            resetPasswordExpires:{$gt: Date.now()}
+        })
+        
+        if(!user){
+            return res.status(400).json({message: "Invalid or expired token"})
+        }
+
+        const newPasswordHash=await hashPassword(password);
+        user.password=newPasswordHash;
+        user.resetPasswordToken=undefined;
+        user.resetPasswordExpires=undefined;
+
+        user.tokenVersion=user.tokenVersion+1;
+
+        await user.save();
+
+        return res.status(200).json({message: "Password reset successfully"})
+    
+
+
+    } catch(err){
+        console.log(err);
+        res.status(500).json({
+            message: 'internal server error'
+    })
+    }
 }
--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { verifyEmailHandler,registerHandler,LoginHandler, refreshHandler, logoutHandler } from "../controllers/auth/auth.controller";
+import { verifyEmailHandler,registerHandler,LoginHandler, refreshHandler, logoutHandler, forgotPasswordHandler, resetPasswordHandler } from "../controllers/auth/auth.controller";
 
 const router=Router();
 
@@ -8,5 +8,7 @@ router.post("/login",LoginHandler);
 router.get("/verify-email",verifyEmailHandler);
 router.post("/refreshToken",refreshHandler);
 router.post("/logout",logoutHandler);
+router.post("/forgot-password",forgotPasswordHandler);
+router.post("/reset-password",resetPasswordHandler);
 
 export default router;
